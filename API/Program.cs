d42e08d505d1061de1694515880f4e8a5f745d9e
Lean <mijareslean@gmail.com>
2026-03-12 21:21:45 +0800

    Section 6 Changes

--- API/Program.cs
+++ API/Program.cs
@@ -36,7 +36,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod()
-    .WithOrigins("http://localhost:4200", "http://localhost:4200" ));
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
