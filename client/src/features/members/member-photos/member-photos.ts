8fdf18f18e97bc68c1e56cebaf3b41b9ffb47d28
mjrslean <mijareslean@gmail.com>
2026-08-21 17:10:14 +0800

    Section 11 Changes

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -1,29 +1,72 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Photo } from '../../../types/members';
-import { AsyncPipe } from '@angular/common';
+import { Member, Photo } from '../../../types/members';
+import { ImageUpload } from '../../../shared/image-upload/image-upload';
+import { AccountService } from '../../../core/services/account-service';
+import { User } from '../../../types/user';
+import { StarButton } from '../../../shared/star-buttton/star-button';
+import { DeleteButton } from '../../../shared/delete-buttton/delete-button';
 
 @Component({
   selector: 'app-member-photos',
-  imports: [AsyncPipe],
+  imports: [ImageUpload, StarButton, DeleteButton],
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css',
 })
-export class MemberPhotos {
-  private memberService = inject(MemberService);
+export class MemberPhotos implements OnInit {
+  protected memberService = inject(MemberService);
+  protected accountService = inject(AccountService);
   private route = inject(ActivatedRoute);
-  protected photos$?: Observable<Photo[]>;
+  protected photos = signal<Photo[]>([]);
+  protected loading = signal(false);
 
-  constructor() {
+  ngOnInit(): void {
     const memberId = this.route.parent?.snapshot.paramMap.get('id');
     if (memberId) {
-      this.photos$ = this.memberService.getMemberPhotos(memberId);
+      this.memberService.getMemberPhotos(memberId).subscribe({
+        next: (photos) => this.photos.set(photos),
+      });
     }
   }
 
-  get photoMocks() {
-    return Array.from({length: 20}, (_, i) => ({id: i, url: `https://picsum.photos/200/200?random=${i}`}));
+  onUploadImage(file: File) {
+    this.loading.set(true);
+    this.memberService.uploadPhoto(file).subscribe({
+      next: (photo) => {
+        this.memberService.editMode.set(false);
+        this.loading.set(false);
+        this.photos.update((photos) => [...photos, photo]);
+      },
+      error: (error) => {
+        console.log('Error uploading image', error);
+        this.loading.set(false);
+      },
+    });
+  }
+
+  setMainPhoto(photo: Photo) {
+    this.memberService.setMainPhoto(photo).subscribe({
+      next: () => {
+        const currentUser = this.accountService.currentUser();
+        if (currentUser) currentUser.imageUrl = photo.url;
+        this.accountService.setCurrentUser(currentUser as User);
+        this.memberService.member.update(
+          (member) =>
+            ({
+              ...member,
+              imageUrl: photo.url,
+            }) as Member,
+        );
+      },
+    });
+  }
+
+  deletePhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe({
+      next: () => {
+        this.photos.update((photos) => photos.filter((x) => x.id !== photoId));
+      },
+    });
   }
 }
